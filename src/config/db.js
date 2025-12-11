431f9fcf53ee89abdcb58bf21db3b5845ea8061b
VAI <kaneong5@gmail.com>
2025-12-12 05:30:05 +0700

    Fix web db connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const dbHost = process.env.DB_HOST === 'localhost' ? '127.0.0.1' : process.env.DB_HOST;
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: dbHost,
   port: process.env.DB_PORT || 3308,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
